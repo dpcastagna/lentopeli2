9413373bde30f8b3c52cdcdd8fc69f47fdd18b0f
Unzila001 <unzilasalman05@gmail.com>
2026-04-27 21:00:36 +0300

    hae_maanosat (app.py,

--- backend/app.py
+++ backend/app.py
@@ -270,4 +270,4 @@ def page_not_found(virhekoodi):
 
 #----------------------SUORITA SOVELLUS-----------------------#
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3000)
+    app.run(host='127.0.0.1', port=3000, use_reloader=False)
